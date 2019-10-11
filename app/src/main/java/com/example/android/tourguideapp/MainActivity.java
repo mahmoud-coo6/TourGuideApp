e07719d373da839259924203cb03cab74149d1cc
Mahmoud Abdo <mahmoudcoo6@gmail.com>
2019-10-11 17:02:24 +0300

    second commit fix

--- app/src/main/java/com/example/android/tourguideapp/MainActivity.java
+++ app/src/main/java/com/example/android/tourguideapp/MainActivity.java
@@ -14,13 +14,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ViewPager viewPager = findViewById(R.id.viewpager);
+//        ViewPager viewPager = findViewById(R.id.swipePager);
+//
+        CustomViewPager mViewPager;
+        mViewPager = (CustomViewPager) findViewById(R.id.swipePager);
+        mViewPager.setOffscreenPageLimit(0);
         SectionAdapter adapter = new SectionAdapter(this, getSupportFragmentManager());
         // Set the adapter onto the view pager
-        viewPager.setAdapter(adapter);
+        mViewPager.setAdapter(adapter);
 
-        TabLayout tabLayout = findViewById(R.id.tabs);
-
-        tabLayout.setupWithViewPager(viewPager);
+//        TabLayout tabLayout = findViewById(R.id.tabs);
+//
+//        tabLayout.setupWithViewPager(viewPager);
     }
 }
